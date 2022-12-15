025f37daa98f2e614846952c6bff8e3ae6f3a6fa
Maxim Semikov <maxim.semikov87@gmail.com>
2022-12-15 11:47:02 +0600

    add: get users

--- src/Components/Сircle/Сircle.js
+++ src/Components/Сircle/Сircle.js
@@ -1,46 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {onSnapshot } from "firebase/firestore"; 
 import {Container, Item, Item2} from "./styles";
-
-
-const imgs = [
-    {
-        src: './images/wow.png',
-        alt: 'image alt text'
-    },
-    {
-        src: './logo.svg',
-        alt: 'image alt text'
-    },
-    {
-        src: './logo.svg',
-        alt: 'image alt text'
-    },
-    {
-        src: './images/wow.png',
-        alt: 'image alt text'
-    },
-    {
-        src: './logo.svg',
-        alt: 'image alt text'
-    },
-    {
-        src: './logo.svg',
-        alt: 'image alt text'
-    },
-];
+import {usersCollectionsRef} from "../../firebase-config";
 
 
 export const Circle = () => {
-    const avatarsCount = imgs.length;
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        return onSnapshot(usersCollectionsRef, (snapshot) => {
+            setUsers(snapshot.docs.map(doc => ({id: doc.id, ...doc.data()})));
+        });
+    }, [])
+
+
+    const avatarsCount = users.length < 8 ? 8 : users.length;
     const tan = Math.tan(Math.PI/avatarsCount);
 
     return (
             <Container m={avatarsCount} tan={tan}>
-                {imgs.map((img, index) => (
-                    <Item key={index} i={index}>
+                {users.map((user, index) => (
+                    <Item key={user.id} i={index}>
                         <Item2>
                             <img src={'https://firebasestorage.googleapis.com/v0/b/happy-new-c414d.appspot.com/o/avatars%2Ftrt-1670877017027?alt=media&token=cd4db903-284f-4bc9-aa95-25fdc95517e0'}
-                                 alt={img.alt}
+                                 alt={user.name}
                                  className={'img'}
                             />
                         </Item2>
